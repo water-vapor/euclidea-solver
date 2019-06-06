f27c37ef121c0f0b46fca776dcb95de14ee00e5d
vapor <zzhaoah@connect.ust.hk>
2019-06-06 15:44:31 -0500

    gofmt

--- configs/configs.go
+++ configs/configs.go
@@ -2,17 +2,17 @@ package configs
 
 const (
 	// HashPrecision is the inverse of Tolerance
-	HashPrecision    = 100000000
+	HashPrecision = 100000000
 	// Prime is the coefficient used in hash function
-	Prime            = 19260817
+	Prime = 19260817
 	// Tolerance is the value below which two values are considered the same
-	Tolerance        = 1e-8
+	Tolerance = 1e-8
 	// EarlyStop determines if we check at early levels for solution
-	EarlyStop        = true
+	EarlyStop = true
 	// ImageSize is the resolution of the output image
-	ImageSize        = 1000
+	ImageSize = 1000
 	// MaxPointCoord is maximum coordinate of a point to be displayed on the image
-	MaxPointCoord    = 1e3
+	MaxPointCoord = 1e3
 	// RandomPointRange is the initiating coordinate range of a random point
 	RandomPointRange = 10
 )
